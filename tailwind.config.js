7e9af3bcaef10562f1665ad88710671350641dde
Ricardo Hernandez <richhquim@gmail.com>
2025-04-08 10:36:11 -0600

    commitmp

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                dark: "#0D1117",
+                light: "#FFFFFF",
+                highlight: "#E3FF6A",
+                neonGreen: "#A4FF7A",
+                deepBlue: "#1E293B",
+                softGray: "#8892B0",
+            },
+            backgroundImage: {
+                "gradient-main": "linear-gradient(135deg, #1E293B 0%, #0D1117 100%)",
+            },
+        },
+    },
+    plugins: [],
+};
